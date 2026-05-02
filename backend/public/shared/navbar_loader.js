74b07ffbf1e1196b007cf36c472533cce5949986
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-05-02 14:45:26 +1200

    Add Browse Recipes navbar link after Food Room

--- backend/public/shared/navbar_loader.js
+++ backend/public/shared/navbar_loader.js
@@ -1,6 +1,6 @@
 // Dynamically loads the enhanced navbar and its assets into #navbar-include.
 (function() {
-  var ASSET_VERSION = '20260502a';
+  var ASSET_VERSION = '20260502b';
   var NAVBAR_URL = '/navbar_enhanced.html?v=' + ASSET_VERSION;
   var STYLE_HREFS = [
     '/navbar.css'
