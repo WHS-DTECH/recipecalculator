4afdb287f932bc82ad28f3a1a1a6e4fa816f2ad2
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-05-06 21:34:21 +1200

    Bump navbar asset version to force menu cache refresh

--- backend/public/shared/navbar_loader.js
+++ backend/public/shared/navbar_loader.js
@@ -1,6 +1,6 @@
 // Dynamically loads the enhanced navbar and its assets into #navbar-include.
 (function() {
-  var ASSET_VERSION = '20260506c';
+  var ASSET_VERSION = '20260506d';
   var NAVBAR_URL = '/navbar2.html?v=' + ASSET_VERSION;
   var STYLE_HREFS = [
     '/navbar2.css'
